bc615bd494aa542f9eaa4941904c2e1b8c1d7175
ElisioWander <elisio741@hotmail.com>
2022-03-16 01:49:25 -0300

    lazy-loading

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,4 +1,23 @@
-import { memo, useMemo } from 'react'
+import { memo, useMemo, useState } from "react";
+import { AddProductToWishListProps } from "./AddProductToWishList"
+
+import dynamic from "next/dynamic";
+
+//o dynamic funciona como algo chamado lazy loading(
+  //Lazy loading é um padrão de projeto de software, comumente utilizado
+  //em linguagens de programação, para adiar a inicialização de um objeto até 
+  //o ponto em que ele é necessário. Isso pode contribuir para a eficiência 
+  //no funcionamento de um programa, se utilizado adequadamente)
+const AddProductToWishList = dynamic<AddProductToWishListProps>(() => {
+  return import('./AddProductToWishList')
+  .then(mod => mod.AddProductToWishList)
+}, {
+  loading: () => <span>carregando...</span> 
+  //esse segundo parâmetro funciona para que caso a conexão do usuário seja ruim
+  //e demore para o componente aparecer quando ele executar uma função que chame
+  //o component, possa ter alguma mensagem ou um spinner, por exemplo, para que o
+  //usuário não fique com uma sensação de que não executou nada. 
+})
 
 interface ProductItemProps {
   product: {
@@ -10,17 +29,30 @@ interface ProductItemProps {
   onAddToWishList: (id: number) => void;
 }
 
-export function ProductItemComponent({ product, onAddToWishList }: ProductItemProps) {
+export function ProductItemComponent({
+  product,
+  onAddToWishList,
+}: ProductItemProps) {
+  const [isAddingToWishList, setIsAddingToWishList] = useState(false);
+
   return (
     <div>
-        <button onClick={() => onAddToWishList(product.id)}></button>
-        {product.title} - <strong>{product.priceFormatted}</strong>
+      {product.title} - <strong>{product.priceFormatted}</strong>
+      <button onClick={() => setIsAddingToWishList(true)}>
+        Add to wishlist
+      </button>
+      {isAddingToWishList && (
+        <AddProductToWishList
+          onAddToWishList={() => onAddToWishList(product.id)}
+          onRequestClose={() => setIsAddingToWishList(false)}
+        />
+      )}
     </div>
   );
 }
 
 //comparação rasa (Shallow Compare)
-export const ProductItem = memo(ProductItemComponent)
+export const ProductItem = memo(ProductItemComponent);
 
 //Comparação mais profunda (Deep Compare)
 // export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
